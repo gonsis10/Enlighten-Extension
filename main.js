d3aa296ad63d634bbb71217cd1ec3376b86cbbde
gonsis10 <simoonsong1006@gmail.com>
2022-12-31 23:32:52 -0800

    Added quote api.

--- main.js
+++ main.js
@@ -1,7 +1,27 @@
 function setTime() {
   const time = dayjs();
-  document.getElementById("clock").innerHTML = time.format("h:mm");
-  document.getElementById("date").innerHTML = time.format("dddd, MMMM D");
+  document.getElementById('clock').innerHTML = time.format('h:mm');
+  document.getElementById('date').innerHTML = time.format('dddd, MMMM D');
 }
-window.onload = setTime();
+
+function setWeather() {
+  null;
+}
+
+async function setQuote() {
+  try {
+    const resp = await fetch('https://api.quotable.io/random?tags=wisdom');
+    const data = await resp.json();
+    document.getElementById('content').innerHTML = `"${data.content}"`;
+    document.getElementById('author').innerHTML = data.author;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+window.onload = () => {
+  setTime();
+  setWeather();
+  setQuote();
+};
 setInterval(setTime, 1000);
